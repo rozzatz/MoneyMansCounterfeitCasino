2d4a38a9a49f8469f294a22b3b95f918d2902d2d
Hyunmin Park <753429@student.dusd.net>
2025-04-09 15:29:42 -0700

    added the ui background
    it be working boiiiiiii

--- MoneyMansCounterfeitCasino/Assets/Scripts/TempAmeobScript.cs
+++ MoneyMansCounterfeitCasino/Assets/Scripts/TempAmeobScript.cs
@@ -1,12 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class TempAmeobScript : MonoBehaviour
 {
     private GameManager gm;
-
+    public GameObject infoBackground;
     
 
     private LootTable loot;
@@ -33,13 +34,26 @@ public class TempAmeobScript : MonoBehaviour
 
     public void OnMouseDown()
     {
-        ameobAmount -= 1;
-        loot.BasicLootCalc();
-        gm.coins += loot.lootResult;
+        if (infoBackground.activeInHierarchy == true)
+        {
+            infoBackground.SetActive(false);
+        }
+        else if(infoBackground.activeInHierarchy == false)
+        {
+            infoBackground.SetActive(true);
+        }
+        
+        infoBackground.transform.SetParent(gameObject.transform);
+        infoBackground.transform.position = new Vector3(gameObject.transform.position.x + 1, gameObject.transform.position.y + 2, gameObject.transform.position.z);
+        
+        
     }
 
     public void TapAmeob()
     {
         //transfer onmousedown here later
+        ameobAmount -= 1;
+        loot.BasicLootCalc();
+        gm.coins += loot.lootResult;
     }
 }
