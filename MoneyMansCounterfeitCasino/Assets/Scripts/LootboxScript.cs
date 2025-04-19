415322bc4af034764f08e3b796000776219275b4
Hyunmin Park <145077936+BaldZero@users.noreply.github.com>
2025-04-18 19:44:27 -0700

    loot box rarities have been updated
    just need to add the ameobs

--- MoneyMansCounterfeitCasino/Assets/Scripts/LootboxScript.cs
+++ MoneyMansCounterfeitCasino/Assets/Scripts/LootboxScript.cs
@@ -1,20 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using Unity.VisualScripting;
 using UnityEngine;
 
 public class LootboxScript : MonoBehaviour
 {
-    public GameObject[] possibleAmeobs;
+    public GameObject[] ameobsTypeOne;
+    public GameObject[] rareAmeobs;
+    public GameObject uniqueAmeob;
+    public GameObject[] ameobsTypeTwo;
     public float cost;
-    public float rarity;
-    public Transform spawnPos;
-    public bool basicAmeob;
+    
+    //public Transform spawnPos;
+    
     public bool basicLoot;
-    public bool rareLoot;
-    public bool abnormalLoot;
     public bool extraordinaryLoot;
     public bool moneyManLoot;
+
+    public float[] lootType;
+    public float[] rareLoot;
+    public float uniqueLoot;
+    public float[] extraLoot;
 
     private GameManager gm;
 
@@ -39,18 +46,74 @@ public class LootboxScript : MonoBehaviour
             //takes away coins and then randomly picks a value from 1 - 100
             gm.coins -= cost;
             float roll = Random.Range(1, 100);
-            if(roll > rarity)
+            if (basicLoot)
             {
-                Debug.Log("Confetti");
+                if (roll <= lootType.Max() && roll >= lootType.Min())
+                {
+                    int ameobRoll = Random.Range(0, ameobsTypeOne.Length);
+                    ameobsTypeOne[ameobRoll].SetActive(true);
+                    TempAmeobScript rolledAmeob = ameobsTypeOne[ameobRoll].GetComponent<TempAmeobScript>();
+                    rolledAmeob.ameobAmount++;
+                }
+                else if(roll <= rareLoot.Max() && roll >= rareLoot.Min())
+                {
+                    int rareameobRoll = Random.Range(0, rareAmeobs.Length);
+                    rareAmeobs[rareameobRoll].SetActive(true);
+                    TempAmeobScript rolledAmeob = rareAmeobs[rareameobRoll].GetComponent<TempAmeobScript>();
+                    rolledAmeob.ameobAmount++;
+                }
+                else if(roll == uniqueLoot)
+                {
+                    uniqueAmeob.SetActive(true);
+                    TempAmeobScript unique = uniqueAmeob.GetComponent<TempAmeobScript>();
+                    unique.ameobAmount++;
+                }
             }
-            else if(roll <= rarity)
+            else if (extraordinaryLoot)
             {
-                int ameobRoll = Random.Range(0, possibleAmeobs.Length);
-                possibleAmeobs[ameobRoll].SetActive(true);
-                TempAmeobScript rolledAmeob = possibleAmeobs[ameobRoll].GetComponent<TempAmeobScript>();
-                rolledAmeob.ameobAmount++;
-
+                if (roll <= lootType.Max() && roll >= lootType.Min())
+                {
+                    int ameobRoll = Random.Range(0, ameobsTypeOne.Length);
+                    ameobsTypeOne[ameobRoll].SetActive(true);
+                    TempAmeobScript rolledAmeob = ameobsTypeOne[ameobRoll].GetComponent<TempAmeobScript>();
+                    rolledAmeob.ameobAmount++;
+                }
+                else if (roll <= rareLoot.Max() && roll >= rareLoot.Min())
+                {
+                    int rareameobRoll = Random.Range(0, rareAmeobs.Length);
+                    rareAmeobs[rareameobRoll].SetActive(true);
+                    TempAmeobScript rolledAmeob = rareAmeobs[rareameobRoll].GetComponent<TempAmeobScript>();
+                    rolledAmeob.ameobAmount++;
+                }
+                else if(roll<= extraLoot.Max() && roll >= extraLoot.Min())
+                {
+                    int extraameobRoll = Random.Range(0, ameobsTypeTwo.Length);
+                    rareAmeobs[extraameobRoll].SetActive(true);
+                    TempAmeobScript rolledAmeob = rareAmeobs[extraameobRoll].GetComponent<TempAmeobScript>();
+                    rolledAmeob.ameobAmount++;
+                }
+                else if (roll == uniqueLoot)
+                {
+                    uniqueAmeob.SetActive(true);
+                    TempAmeobScript unique = uniqueAmeob.GetComponent<TempAmeobScript>();
+                    unique.ameobAmount++;
+                }
             }
+            else if (moneyManLoot)
+            {
+                if(roll <= lootType.Max() && roll >= lootType.Min())
+                {
+                    Debug.Log("CONFETTI");
+                }
+                else if(roll == uniqueLoot)
+                {
+                    uniqueAmeob.SetActive(true);
+                    TempAmeobScript unique = uniqueAmeob.GetComponent<TempAmeobScript>();
+                    unique.ameobAmount++;
+                }
+            }
+            
+            
         }
         else
         // if the player does not have enough coins this happens
